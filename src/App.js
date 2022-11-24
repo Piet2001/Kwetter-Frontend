f99526170bb7f633763daa0f274d07b43b2f8f2b
Piet2001 <piet2001@fireman.net>
2022-11-24 12:23:38 +0100

    add azure login

--- src/App.js
+++ src/App.js
@@ -2,24 +2,45 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import Home from "./components/Home/Home"
 import React from "react";
+import {
+  useIsAuthenticated,
+} from '@azure/msal-react';
+
 import './App.css';
 import NotFound from "./components/Default/NotFound";
 import Profile from "./components/Profile/Profile";
+import Login from "./components/Default/Login";
+
 
 function App() {
+
+  const ProtectedRoute = ({ children }) => {
+    console.log(useIsAuthenticated())
+    if (!useIsAuthenticated()) {
+      return <Layout><Login /></Layout>;
+    }
+
+    return children;
+  };
 
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/Kwetter-Frontend' element={
-          <Layout>
-            <Home />
-          </Layout>}
+          <ProtectedRoute>
+            <Layout>
+              <Home />
+            </Layout>
+          </ProtectedRoute>
+        }
         />
         <Route path='/Kwetter-Frontend/profile' element={
-          <Layout>
-            <Profile />
-          </Layout>}
+          <ProtectedRoute>
+            <Layout>
+              <Profile />
+            </Layout>
+          </ProtectedRoute>
+        }
         />
         <Route path="*" element={
           <Layout>
